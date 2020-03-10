f11ecd3cb75f499b598c80a1e7b97e4252dd10b7
Tabassum Hassan <tiram819@gmail.com>
2020-03-10 20:25:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,14 @@
-def prime(num)
-  i = 0
-  while i < num.length
-    return num[i] if yield(num[i])
-    i = i + 1
-  end
+def prime(n)
+prime_array = []    
+i = 2
+if n < 2
+    return i
 end
+
+while (n % p == 0) && (i < n)
+    prime_array.push(i)
+    i += 1
+end
+return prime_array
+end
+
